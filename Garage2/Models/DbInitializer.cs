d2f28a9ab2225900c8e7c0419160ce25c01f54d7
Alexander Öberg <stativetroller7@gmail.com>
2023-11-02 17:01:04 +0100

    create controller

--- Garage2/Models/DbInitializer.cs
+++ Garage2/Models/DbInitializer.cs
@@ -1,68 +1,68 @@
-﻿using Garage2.Data;
-using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+﻿//using Garage2.Data;
+//using Microsoft.CodeAnalysis.Elfie.Diagnostics;
 
-namespace Garage2.Models
-{
-    public static class DbInitializer
-    {
-        public static void Seed(IApplicationBuilder applicationBuilder)
-        {
-            Garage2Context _context = applicationBuilder.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<Garage2Context>();
+//namespace Garage2.Models
+//{
+//    public static class DbInitializer
+//    {
+//        public static void Seed(IApplicationBuilder applicationBuilder)
+//        {
+//            Garage2Context _context = applicationBuilder.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<Garage2Context>();
 
-            if (!_context.ParkedVehicle.Any())
-            {
-                _context.AddRange(
-                    new ParkedVehicle(
-                        registrationNumber: new RegistryNumber(NationCode.SE, "XTV213"),
-                        vehicleType: VehicleType.B,
-                        color: "Silver",
-                        brand: "Volkswagen",
-                        model: "Golf",
-                        numberOfWheels: 4,
-                        arrivalTime: new DateTime(2019, 01, 23)),
-                    new ParkedVehicle(
-                        registrationNumber: new RegistryNumber(NationCode.SE, "ABC123"),
-                        vehicleType: VehicleType.B, 
-                        color: "Blue", 
-                        brand: "Ford", 
-                        model: "Mustang", 
-                        numberOfWheels: 4,
-                        arrivalTime: new DateTime(2020, 05, 15)),
-                    new ParkedVehicle(
-                        registrationNumber: new RegistryNumber(NationCode.SE, "XYZ789"),
-                        vehicleType: VehicleType.B, 
-                        color: "Red", 
-                        brand: "Toyota", 
-                        model: "Camry", 
-                        numberOfWheels: 4,
-                        arrivalTime: new DateTime(2021, 11, 10)),
-                    new ParkedVehicle(
-                        registrationNumber: new RegistryNumber(NationCode.SE, "MOT456"),
-                        vehicleType: VehicleType.A, 
-                        color: "Black", 
-                        brand: "Honda", 
-                        model: "CBR600RR",
-                        numberOfWheels: 2, 
-                        arrivalTime: new DateTime(2020, 07, 12)),
-                    new ParkedVehicle(
-                        registrationNumber: new RegistryNumber(NationCode.SE, "TRK001"),
-                        vehicleType: VehicleType.C, 
-                        color: "White", 
-                        brand: "Ford", 
-                        model: "F-150",
-                        numberOfWheels: 6, // Trucks often have multiple wheels
-                        arrivalTime: new DateTime(2019, 08, 05)),
-                    new ParkedVehicle(
-                        registrationNumber: new RegistryNumber(NationCode.SE, "TRK002"),
-                        vehicleType: VehicleType.C, 
-                        color: "Blue", 
-                        brand: "Chevrolet", 
-                        model: "Silverado",
-                        numberOfWheels: 8, // Larger trucks might have more wheels
-                        arrivalTime: new DateTime(2020, 03, 15)));
-            }
+//            if (!_context.ParkedVehicle.Any())
+//            {
+//                _context.AddRange(
+//                    new ParkedVehicle(
+//                        registrationNumber: new RegistryNumber(NationCode.SE, "XTV213"),
+//                        vehicleType: VehicleType.B,
+//                        color: "Silver",
+//                        brand: "Volkswagen",
+//                        model: "Golf",
+//                        numberOfWheels: 4,
+//                        arrivalTime: new DateTime(2019, 01, 23)),
+//                    new ParkedVehicle(
+//                        registrationNumber: new RegistryNumber(NationCode.SE, "ABC123"),
+//                        vehicleType: VehicleType.B, 
+//                        color: "Blue", 
+//                        brand: "Ford", 
+//                        model: "Mustang", 
+//                        numberOfWheels: 4,
+//                        arrivalTime: new DateTime(2020, 05, 15)),
+//                    new ParkedVehicle(
+//                        registrationNumber: new RegistryNumber(NationCode.SE, "XYZ789"),
+//                        vehicleType: VehicleType.B, 
+//                        color: "Red", 
+//                        brand: "Toyota", 
+//                        model: "Camry", 
+//                        numberOfWheels: 4,
+//                        arrivalTime: new DateTime(2021, 11, 10)),
+//                    new ParkedVehicle(
+//                        registrationNumber: new RegistryNumber(NationCode.SE, "MOT456"),
+//                        vehicleType: VehicleType.A, 
+//                        color: "Black", 
+//                        brand: "Honda", 
+//                        model: "CBR600RR",
+//                        numberOfWheels: 2, 
+//                        arrivalTime: new DateTime(2020, 07, 12)),
+//                    new ParkedVehicle(
+//                        registrationNumber: new RegistryNumber(NationCode.SE, "TRK001"),
+//                        vehicleType: VehicleType.C, 
+//                        color: "White", 
+//                        brand: "Ford", 
+//                        model: "F-150",
+//                        numberOfWheels: 6, // Trucks often have multiple wheels
+//                        arrivalTime: new DateTime(2019, 08, 05)),
+//                    new ParkedVehicle(
+//                        registrationNumber: new RegistryNumber(NationCode.SE, "TRK002"),
+//                        vehicleType: VehicleType.C, 
+//                        color: "Blue", 
+//                        brand: "Chevrolet", 
+//                        model: "Silverado",
+//                        numberOfWheels: 8, // Larger trucks might have more wheels
+//                        arrivalTime: new DateTime(2020, 03, 15)));
+//            }
 
-            _context.SaveChanges();
-        }
-    }
-}
+//            _context.SaveChanges();
+//        }
+//    }
+//}
--- Garage2/Models/ParkedVehicle.cs
+++ Garage2/Models/ParkedVehicle.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Garage2.Models;
 public enum VehicleType
@@ -18,46 +19,55 @@ public enum VehicleType
     DE,
 }
 
-
-public readonly record struct RegistryNumber
+public static class RegistryNumber
 {
-    public RegistryNumber(NationCode Nationality, string SerialNumber)
-    {
-        this.Nationality = Nationality;
-        // ensure serial number contains only letters, digits and hyphens
-        if (SerialNumber.Any(c => !(char.IsLetterOrDigit(c) || c == '-')))
-        {
-            throw new ArgumentException("Serial number must contain only letters, digits and hyphens");
-        }
-        this.SerialNumber = SerialNumber.ToUpperInvariant();
-    }
-    public NationCode Nationality { get; }
-    public string SerialNumber { get; }
+    //public RegistryNumber(NationCode Nationality, string SerialNumber)
+    //{
+    //    this.Nationality = Nationality;
+    //    // ensure serial number contains only letters, digits and hyphens
+    //    if (SerialNumber.Any(c => !(char.IsLetterOrDigit(c) || c == '-')))
+    //    {
+    //        throw new ArgumentException("Serial number must contain only letters, digits and hyphens");
+    //    }
+    //    this.SerialNumber = SerialNumber.ToUpperInvariant();
+    //}
 
-    public static implicit operator RegistryNumber(string registryNumberString)
-    {
-        string[] parts = registryNumberString.ToUpperInvariant().Split(' ');
-        if (parts.Length != 2)
-        {
-            throw new ArgumentException("Invalid registry number string format");
-        }
-        NationCode nationality = Enum.Parse<NationCode>(parts[0]);
-        string serialNumber = parts[1];
-        return new RegistryNumber(nationality, serialNumber);
-    }
+    //public RegistryNumber()
+    //{
+        
+    //}
+    //public NationCode Nationality { get; }
+    //public string SerialNumber { get; }
 
-    public override string ToString()
-    {
-        return $"{Nationality} {SerialNumber}";
-    }
+    //[Key]
+    //public string RegNum => ToString();
+
+    //public static ToRegistryNumber(string registryNumberString)
+    //{
+    //    string[] parts = registryNumberString.ToUpperInvariant().Split(' ');
+    //    if (parts.Length != 2)
+    //    {
+    //        throw new ArgumentException("Invalid registry number string format");
+    //    }
+    //    NationCode nationality = Enum.Parse<NationCode>(parts[0]);
+    //    string serialNumber = parts[1];
+    //    return $"{nationality} {serialNumber}";
+    //}
+
+    //public override string ToString()
+    //{
+    //    return $"{Nationality} {SerialNumber}";
+    //}
 
 }
 
 
 public class ParkedVehicle
 {
+    [Key]
     public int Id { get; set; }
-    public RegistryNumber RegistrationNumber { get; set; }
+    //[Remote()]
+    public string RegistrationNumber { get; set; }
     public VehicleType VehicleType { get; set; }
     public string Color { get; set; }
     public string Brand { get; set; }
@@ -65,15 +75,15 @@ public class ParkedVehicle
     public int NumberOfWheels { get; set; }
     public DateTime ArrivalTime { get; set; }
 
-    public ParkedVehicle(RegistryNumber registrationNumber, VehicleType vehicleType, string color, string brand, string model, int numberOfWheels, DateTime arrivalTime)
+}
+
+
+public class CustomValidator : ValidationAttribute
+{
+    public override bool IsValid(object? value)
     {
-        this.RegistrationNumber = registrationNumber;
-        this.VehicleType = vehicleType;
-        this.Color = color;
-        this.Brand = brand;
-        this.Model = model;
-        this.NumberOfWheels = numberOfWheels;
-        this.ArrivalTime = arrivalTime;
+        //
+        return true;
     }
 }
 
